61111e42eb761c7576ff8fa7adc4980e260e6a85
ZhangZifei <1773908404@qq.com>
2022-05-11 13:51:24 +0800

    vcs: add args diff/no-diff/max-cycles

--- src/test/csrc/vcs/main.cpp
+++ src/test/csrc/vcs/main.cpp
@@ -21,10 +21,13 @@
 #include "goldenmem.h"
 #include "ram.h"
 #include "flash.h"
+#include "refproxy.h"
 
 static bool has_reset = false;
 static char bin_file[256] = "ram.bin";
 static char *flash_bin_file = NULL;
+static bool enable_difftest = true;
+static int max_cycles = 0;
 
 extern "C" void set_bin_file(char *s) {
   printf("ram image:%s\n",s);
@@ -37,6 +40,24 @@ extern "C" void set_flash_bin(char *s) {
   strcpy(flash_bin_file, s);
 }
 
+extern "C" void set_diff_ref_so(char *s) {
+  printf("diff-test ref so:%s\n", s);
+  extern const char *difftest_ref_so;
+  char* buf = (char *)malloc(256);
+  strcpy(buf, s);
+  difftest_ref_so = buf;
+}
+
+extern "C" void set_no_diff() {
+  printf("disable diff-test\n");
+  enable_difftest = false;
+}
+
+extern "C" void set_max_cycles(long mc) {
+  printf("max cycles:%d\n", mc);
+  max_cycles = mc;
+}
+
 extern "C" void simv_init() {
   printf("simv compiled at %s, %s\n", __DATE__, __TIME__);
   setlocale(LC_NUMERIC, "");
@@ -46,8 +67,10 @@ extern "C" void simv_init() {
 
   difftest_init();
   init_device();
-  init_goldenmem();
-  init_nemuproxy();
+  if (enable_difftest) {
+    init_goldenmem();
+    init_nemuproxy();
+  }
 
   assert_init();
 }
@@ -56,16 +79,30 @@ extern "C" int simv_step() {
   if (assert_count > 0) {
     return 1;
   }
-  if (difftest_state() != -1) {
-    int trapCode = difftest_state();
-    switch (trapCode) {
-      case 0:
-        eprintf(ANSI_COLOR_GREEN "HIT GOOD TRAP\n" ANSI_COLOR_RESET);
-        break;
-      default:
-        eprintf(ANSI_COLOR_RED "Unknown trap code: %d\n", trapCode);
+
+  static int cycles = 0;
+  if (max_cycles != 0) { // 0 for no limit
+    if (cycles >= max_cycles) {
+      eprintf(ANSI_COLOR_YELLOW "EXCEEDED MAX CYCLE:%d\n" ANSI_COLOR_RESET, max_cycles);
+      return 1;
     }
-    return trapCode + 1;
+    cycles ++;
   }
-  return difftest_step();
+
+  if (enable_difftest) {
+    if (difftest_state() != -1) {
+      int trapCode = difftest_state();
+      switch (trapCode) {
+        case 0:
+          eprintf(ANSI_COLOR_GREEN "HIT GOOD TRAP\n" ANSI_COLOR_RESET);
+          break;
+        default:
+          eprintf(ANSI_COLOR_RED "Unknown trap code: %d\n" ANSI_COLOR_RESET, trapCode);
+      }
+      return trapCode + 1;
+    }
+    return difftest_step();
+  } else {
+    return 0;
+  }
 }
