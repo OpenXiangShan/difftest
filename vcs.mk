4e4adbac6cf28388ad6514684496cfa49065a09f
ZhangZifei <1773908404@qq.com>
2022-05-19 20:48:35 +0800

    vcs: fix vcs flag about dump-wave

--- vcs.mk
+++ vcs.mk
@@ -41,7 +41,7 @@ VCS_FLAGS += -y $(VCS_SEARCH_DIR) +libext+.v
 # build files put into $(VCS_BUILD_DIR)
 VCS_FLAGS += -Mdir=$(VCS_BUILD_DIR)
 # enable fsdb dump
-VCS_FLAGS += -debug_access+all
+VCS_FLAGS += -debug_access+all -fsdb
 
 $(VCS_TARGET): $(SIM_TOP_V) $(VCS_CXXFILES) $(VCS_VFILES)
 	vcs $(VCS_FLAGS) $(SIM_TOP_V) $(VCS_CXXFILES) $(VCS_VFILES)
