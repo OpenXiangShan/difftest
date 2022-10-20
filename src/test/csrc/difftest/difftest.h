df1a700a78e2614ea4ae07019d90feb2737edca6
Yinan Xu <xuyinan@ict.ac.cn>
2022-10-20 17:47:30 +0800

    diff: enlarge stuck time limit

--- src/test/csrc/difftest/difftest.h
+++ src/test/csrc/difftest/difftest.h
@@ -363,7 +363,7 @@ public:
 
 protected:
   const uint64_t firstCommit_limit = 10000;
-  const uint64_t stuck_limit = 5000;
+  const uint64_t stuck_limit = 10000;
 
   int id;
   difftest_core_state_t dut;
