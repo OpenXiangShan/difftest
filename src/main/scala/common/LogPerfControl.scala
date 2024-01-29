b8ce48d942e2f313e4818ec2abf3aee7d4105ea8
Yinan Xu <xuyinan@ict.ac.cn>
2024-01-29 14:52:33 +0800

    Disable Verilator coverage in LogPerfControl

--- src/main/scala/common/LogPerfControl.scala
+++ src/main/scala/common/LogPerfControl.scala
@@ -34,18 +34,19 @@ private class LogPerfHelper extends BlackBox with HasBlackBoxInline {
       |`endif
       |
       |/*verilator tracing_off*/
+      |/*verilator coverage_off*/
       |
-      |module LogPerfHelper (
+      |module LogPerfHelper(
       |  output [63:0] timer,
       |  output        logEnable,
       |  output        clean,
       |  output        dump
       |);
       |
-      |assign timer         = `SIM_TOP_MODULE_NAME.difftest_timer;
-      |assign logEnable     = `SIM_TOP_MODULE_NAME.difftest_log_enable;
-      |assign clean         = `SIM_TOP_MODULE_NAME.difftest_perfCtrl_clean;
-      |assign dump          = `SIM_TOP_MODULE_NAME.difftest_perfCtrl_dump;
+      |assign timer     = `SIM_TOP_MODULE_NAME.difftest_timer;
+      |assign logEnable = `SIM_TOP_MODULE_NAME.difftest_log_enable;
+      |assign clean     = `SIM_TOP_MODULE_NAME.difftest_perfCtrl_clean;
+      |assign dump      = `SIM_TOP_MODULE_NAME.difftest_perfCtrl_dump;
       |
       |endmodule
       |
