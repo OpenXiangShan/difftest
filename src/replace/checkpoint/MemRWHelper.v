a35df6005677b0334d8d854f56d4f88cf445a611
Kamimiao <1317379456@qq.com>
2024-02-02 14:20:58 +0800

    memrw: add define `MEM_DPIC function

--- src/replace/checkpoint/MemRWHelper.v
+++ src/replace/checkpoint/MemRWHelper.v
@@ -1,5 +1,6 @@
 // The memory used to load the checkpoint
 import "DPI-C" function void ram_read_data(input longint addr,output longint data);
+import "DPI-C" function void ram_write_data(input longint addr,input longint w_mask,input longint data);
 import "DPI-C" function void init_ram(input longint size);
 import "DPI-C" function void ram_set_bin_file(string bin);
 import "DPI-C" function void ram_set_gcpt_bin(string bin);
@@ -22,12 +23,14 @@ input  [63:0] w_mask,
 string bin_file;
 string gcpt_bin_file;
 string mem_record_file;
+
 `define RAM_SIZE_BYTE (8 * 1024 * 1024 * 1024)// 8G
 `define RAM_SIZE_BITS_1G (1 * 1024 * 1024 * 1024)/8// 1G
 `define RAM_SIZE_BITS_2G `RAM_SIZE_BITS_1G * 2
 `define RAM_SIZE_BITS_3G `RAM_SIZE_BITS_1G * 3
 `define RAM_SIZE_BITS_4G `RAM_SIZE_BITS_1G * 4
 
+`ifndef MEM_DPIC
   reg [63:0] memory_1 [0 : `RAM_SIZE_BITS_1G - 1];
   reg [63:0] memory_2 [0 : `RAM_SIZE_BITS_1G - 1];
   reg [63:0] memory_3 [0 : `RAM_SIZE_BITS_1G - 1];
@@ -35,9 +38,12 @@ string mem_record_file;
 
   wire [2:0]r_addr_mux = r_index[29:27];
   wire [2:0]w_addr_mux = w_index[29:27];
+`endif // MEM_DPIC
+
   // memory read
   always @(posedge clock) begin
     if (r_enable && enable) begin    
+`ifndef MEM_DPIC
       case (r_addr_mux)
         'b000: begin r_data <= memory_1[r_index[26:0]]; end
         'b001: begin r_data <= memory_2[r_index[26:0]]; end
@@ -47,12 +53,16 @@ string mem_record_file;
           $fatal(1, "An address that exceeds the memory limit %h",r_index);
         end
       endcase
+`else
+    ram_read_data(r_index,r_data);
+`endif // MEM_DPIC
     end
   end
 
   // memory write
   always @(posedge clock) begin
     if (w_enable && enable) begin
+`ifndef MEM_DPIC
       case (w_addr_mux)
         'b000: begin memory_1[w_index[26:0]] <= (w_data & w_mask) | (memory_1[w_index[26:0]] & ~w_mask); end
         'b001: begin memory_2[w_index[26:0]] <= (w_data & w_mask) | (memory_2[w_index[26:0]] & ~w_mask); end
@@ -62,6 +72,9 @@ string mem_record_file;
           $fatal(1, "An address that exceeds the memory limit %h",w_index);
         end
       endcase
+`else
+      ram_write_data(w_index, w_mask, w_data);
+`endif // MEM_DPIC
     end
   end
 
@@ -83,6 +96,8 @@ string mem_record_file;
     ram_set_gcpt_bin(gcpt_bin_file);
   end
   init_ram(`RAM_SIZE_BYTE);
+
+`ifndef MEM_DPIC
   if ($test$plusargs("mem-record")) begin // use checkpoint compression
     $value$plusargs("mem-record=%s", mem_record_file);
     memory_record_en = 1;
@@ -156,6 +171,6 @@ string mem_record_file;
       ram_read_data(i + `RAM_SIZE_BITS_3G, memory_4[i]);
     end
   end
-
+`endif // MEM_DPIC
   end // end init
 endmodule
