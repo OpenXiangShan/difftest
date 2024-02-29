b3c7883ea7fe2b15bbd9fa376bf8aebf1e604bb6
xiaokamikami <1317379456@qq.com>
2024-02-29 10:59:50 +0800

    zstdlist:update

--- src/test/csrc/checkpoint/get_zstd_list.sh
+++ src/test/csrc/checkpoint/get_zstd_list.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 CHECKPOINT_PATH="/nfs/home/fengkehan/zstd-test"
-CHECKPOINT_LIST="$CHECKPOINT_PATH/list.txt"
+CHECKPOINT_LIST="$CHECKPOINT_PATH/list-test.txt"
 
 if [ ! -f "$CHECKPOINT_LIST" ]; then
     touch "$CHECKPOINT_LIST"
@@ -12,12 +12,16 @@ do
         var=$file
         work_load_name=${var#$CHECKPOINT_PATH/}
         work_load_name=${work_load_name%/*}
+        IFS='/' read -ra ADDR <<< "$work_load_name"  
+        work_load_name=${ADDR[0]}
+        prefix=${ADDR[1]}
+
         var=${var##*/}
         var=${var%.zstd}
 
-        prefix=${var%%.*}  # Get the first dot (.) The previous section
+        #prefix=${var%%.*}  # Get the first dot (.) The previous section
         suffix=${var#*.}   # Get the first dot (.) The next part
         suffix=$(echo $suffix | sed 's/[^0-9]//g')
-        printf "%s %s\n" $file $suffix >> "$CHECKPOINT_LIST"
+        printf "%s %s %s %s %s\n" $file $suffix $work_load_name $prefix >> "$CHECKPOINT_LIST"
     fi
 done
