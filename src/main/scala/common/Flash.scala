3120554918cbc49348a6a7774e9905afcc9a9be3
klin02 <youkunlin20@mails.ucas.ac.cn>
2024-03-06 22:35:15 +0800

    flash: support synthesizable flash without SYNTHESIS
    Previously we use synthesizable flash only when SYNTHESIS=1, now
    we support configuring synthesizable flash just like memory,
    so we can use it to run workload based on flash.

--- src/main/scala/common/Flash.scala
+++ src/main/scala/common/Flash.scala
@@ -32,13 +32,16 @@ class FlashHelper extends ExtModule with HasExtModuleInline {
   setInline(
     "FlashHelper.v",
     """
-      |`ifndef SYNTHESIS
+      |`ifdef SYNTHESIS
+      |  `define DISABLE_DIFFTEST_FLASH_DPIC
+      |`endif // SYNTHESIS
+      |`ifndef DISABLE_DIFFTEST_FLASH_DPIC
       |import "DPI-C" function void flash_read
       |(
       |  input int addr,
       |  output longint data
       |);
-      |`endif // SYNTHESIS
+      |`endif // DISABLE_DIFFTEST_FLASH_DPIC
       |
       |module FlashHelper (
       |  input clock,
@@ -47,7 +50,7 @@ class FlashHelper extends ExtModule with HasExtModuleInline {
       |  output reg [63:0] r_data
       |);
       |
-      |`ifndef SYNTHESIS
+      |`ifndef DISABLE_DIFFTEST_FLASH_DPIC
       |  always @(posedge clock) begin
       |    if (r_en) flash_read(r_addr, r_data);
       |  end
@@ -55,8 +58,8 @@ class FlashHelper extends ExtModule with HasExtModuleInline {
       |`ifdef PALLADIUM
       |  initial $ixc_ctrl("tb_import", "$display");
       |`endif // PALLADIUM
-      |  // 1K entries. 8KB size.
-      |  `define FLASH_SIZE (8 * 1024)
+      |  // 512K entries. 4MB size.
+      |  `define FLASH_SIZE (4 * 1024 * 1024)
       |  reg [7:0] flash_mem [0 : `FLASH_SIZE - 1];
       |
       |  for (genvar i = 0; i < 8; i++) begin
@@ -103,7 +106,7 @@ class FlashHelper extends ExtModule with HasExtModuleInline {
       |      end
       |    end
       |  end
-      |`endif // SYNTHESIS
+      |`endif // DISABLE_DIFFTEST_FLASH_DPIC
       |
       |endmodule
      """.stripMargin,
