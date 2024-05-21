66f33c94891e36bdd58ed39d98ece520cf4c723c
ZhangZifei <zhangzifei16@mails.ucas.ac.cn>
2024-05-21 11:20:34 +0800

    tracertl: add dummy trace icache class

---
+++ src/test/csrc/tracertl/trace_icache.cpp
@@ -0,0 +1,83 @@
+/***************************************************************************************
+* Copyright (c) 2020-2023 Institute of Computing Technology, Chinese Academy of Sciences
+* Copyright (c) 2020-2021 Peng Cheng Laboratory
+*
+* DiffTest is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#include <fstream>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <iostream>
+#include "trace_icache.h"
+
+TraceICache::TraceICache(const char *binary_name) {
+  // uint64_t memory_size = 8 * 1024 * 1024 * 1024UL; // 8GB
+  fd = open(binary_name, O_RDONLY);
+  if (fd == -1) {
+    perror("open file failed.");
+    exit(EXIT_FAILURE);
+    return;
+  }
+  struct stat sb;
+  if (fstat(fd, &sb) == -1) {
+    perror("fstat failed.");
+    close(fd);
+    exit(EXIT_FAILURE);
+  }
+
+  mmap_size = sb.st_size;
+  ram_size = mmap_size;
+  ram = (char *)mmap(NULL, mmap_size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+  if (ram == MAP_FAILED) {
+    perror("mmap failed.");
+    close(fd);
+    exit(EXIT_FAILURE);
+  }
+
+  base_addr = 0x80000000; // 2GB
+}
+
+bool TraceICache::readCacheLine(char *line, int addr) {
+  if (!legalAddr(addr)) {
+    perror("illegal address");
+    exit(EXIT_FAILURE);
+    return false;
+  }
+
+  int ram_addr = ramAddr(addr);
+  memcpy(line, (char *)(ram + ram_addr), sizeof(TraceCacheLine));
+  return true;
+}
+
+bool TraceICache::readHalfCacheLine(char *line, int addr) {
+  printf("readHalfCacheLine: addr = %x\n", addr);
+  if (!legalAddr(addr)) {
+    perror("illegal address");
+    exit(EXIT_FAILURE);
+    return false;
+  }
+
+  int ram_addr = ramAddr(addr);
+  memcpy(line, (char *)(ram + ram_addr), sizeof(TraceHalfCacheLine));
+  return true;
+}
+
+TraceICache::~TraceICache() {
+  close(fd);
+  munmap(ram, mmap_size);
+}
---
+++ src/test/csrc/tracertl/trace_icache.h
@@ -0,0 +1,56 @@
+/***************************************************************************************
+* Copyright (c) 2020-2023 Institute of Computing Technology, Chinese Academy of Sciences
+* Copyright (c) 2020-2021 Peng Cheng Laboratory
+*
+* DiffTest is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#ifndef __TRACE_ICACHE_H__
+#define __TRACE_ICACHE_H__
+
+#include <cstdint>
+#include <cstdio>
+
+struct TraceCacheLine {
+  uint64_t pc[512 / sizeof(uint64_t)];
+};
+struct TraceHalfCacheLine {
+  uint64_t pc[256 / sizeof(uint64_t)];
+};
+
+class TraceICache {
+
+private:
+  // only for program binary
+  char *ram;
+  int fd;
+  uint64_t mmap_size;
+  uint64_t ram_size;
+  uint64_t base_addr;
+
+public:
+  TraceICache(const char *binary_name);
+  ~TraceICache();
+  bool readCacheLine(char *line, int addr);
+  bool readHalfCacheLine(char *line, int addr);
+  int ramAddr(int addr) {
+    return alignAddr(addr - base_addr);
+  }
+  int alignAddr(int addr) {
+    return addr & ~(sizeof(TraceCacheLine) - 1);
+  }
+  int legalAddr(int addr) {
+    return ((addr >= base_addr) && ((addr < base_addr) + ram_size));
+  }
+};
+
+#endif
