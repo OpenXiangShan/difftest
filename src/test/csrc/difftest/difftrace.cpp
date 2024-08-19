b9da243c1e52c1d4b265b80b1b382ede15886ee6
Kamimiao <1317379456@qq.com>
2024-08-19 12:45:42 +0800

    Trace: support zstd compress/decompress (#439)
    Add ZSTD support for streaming compress/decompress Difftest IOTrace. Enable by set IOTRACE_ZSTD=1.

--- src/test/csrc/difftest/difftrace.cpp
+++ src/test/csrc/difftest/difftrace.cpp
@@ -4,8 +4,8 @@
 
 template <typename T>
 DiffTrace<T>::DiffTrace(const char *_trace_name, bool is_read, uint64_t _buffer_size) : is_read(is_read) {
+  buffer_size = _buffer_size;
   if (!is_read) {
-    buffer_size = _buffer_size;
     buffer = (T *)calloc(buffer_size, sizeof(T));
   }
   if (strlen(trace_name) > 31) {
@@ -14,6 +14,9 @@ DiffTrace<T>::DiffTrace(const char *_trace_name, bool is_read, uint64_t _buffer_
     exit(0);
   }
   strcpy(trace_name, _trace_name);
+#ifdef CONFIG_IOTRACE_ZSTD
+  trace_zstd = new DiffTraceZstd(buffer_size);
+#endif
 }
 
 template <typename T> bool DiffTrace<T>::append(const T *trace) {
@@ -26,7 +29,11 @@ template <typename T> bool DiffTrace<T>::append(const T *trace) {
 }
 
 template <typename T> bool DiffTrace<T>::read_next(T *trace) {
+#ifndef CONFIG_IOTRACE_ZSTD
   if (!buffer || buffer_count == buffer_size) {
+#else
+  if (!buffer || trace_zstd->trace_load_len == buffer_count) {
+#endif // CONFIG_IOTRACE_ZSTD
     trace_file_next();
   }
   memcpy(trace, buffer + buffer_count, sizeof(T));
@@ -35,12 +42,9 @@ template <typename T> bool DiffTrace<T>::read_next(T *trace) {
   return 0;
 }
 
-template <typename T> bool DiffTrace<T>::trace_file_next() {
+template <typename T> void DiffTrace<T>::next_file_name(char *file_name) {
+  memset(file_name, 0, 128);
   static uint64_t trace_index = 0;
-  static FILE *file = nullptr;
-  if (file) {
-    fclose(file);
-  }
   char dirname[128];
   if (strchr(trace_name, '/')) {
     snprintf(dirname, 128, "%s", trace_name);
@@ -49,10 +53,40 @@ template <typename T> bool DiffTrace<T>::trace_file_next() {
     snprintf(dirname, 128, "%s/%s", noop_home, trace_name);
   }
   mkdir(dirname, 0755);
-  char filename[128];
+#ifndef CONFIG_IOTRACE_ZSTD
   const char *prefix = "bin";
-  snprintf(filename, 128, "%s/%lu.%s", dirname, trace_index, prefix);
+#else
+  const char *prefix = "zstd";
+#endif // CONFIG_IOTRACE_ZSTD
+  snprintf(file_name, 128, "%s/%lu.%s", dirname, trace_index, prefix);
+  trace_index++;
+}
+
+template <typename T> bool DiffTrace<T>::trace_file_next() {
+  static char *filename = (char *)malloc(128);
+#ifdef CONFIG_IOTRACE_ZSTD
+  if (trace_zstd->need_load_new_file == true && is_read) {
+    next_file_name(filename);
+    trace_zstd->diff_zstd_next(filename, is_read);
+    trace_zstd->need_load_new_file = false;
+    Info("Loading traces from %s ...\n", filename);
+  } else if (!is_read) {
+    next_file_name(filename);
+    trace_zstd->diff_zstd_next(filename, is_read);
+  }
+#else
+  next_file_name(filename);
+  static FILE *file = nullptr;
+  if (file) {
+    fclose(file);
+  }
+#endif
+
   if (is_read) {
+    if (buffer) {
+      free(buffer);
+    }
+#ifndef CONFIG_IOTRACE_ZSTD
     FILE *file = fopen(filename, "rb");
     if (!file) {
       printf("File %s not found.\n", filename);
@@ -61,29 +95,128 @@ template <typename T> bool DiffTrace<T>::trace_file_next() {
     // check the number of traces
     fseek(file, 0, SEEK_END);
     buffer_size = ftell(file) / sizeof(T);
-    if (buffer) {
-      free(buffer);
-    }
     buffer = (T *)calloc(buffer_size, sizeof(T));
     // read the binary file
-    Info("Loading %lu traces from %s ...\n", buffer_size, filename);
     fseek(file, 0, SEEK_SET);
     uint64_t read_bytes = fread(buffer, sizeof(T), buffer_size, file);
     assert(read_bytes == buffer_size);
     fclose(file);
-    buffer_count = 0;
+    Info("Loading %lu traces from %s ...\n", buffer_size, filename);
+#else
+    buffer = (T *)calloc(buffer_size, sizeof(T));
+    trace_zstd->diff_IOtrace_load((char *)buffer, sizeof(T));
+#endif // CONFIG_IOTRACE_ZSTD
   } else if (buffer_count > 0) {
     Info("Writing %lu traces to %s ...\n", buffer_count, filename);
+#ifndef CONFIG_IOTRACE_ZSTD
     FILE *file = fopen(filename, "wb");
     fwrite(buffer, sizeof(T), buffer_count, file);
     fclose(file);
-    buffer_count = 0;
+#else
+    trace_zstd->diff_IOtrace_dump((char *)buffer, sizeof(T) * buffer_count);
+#endif
   }
-  trace_index++;
+  buffer_count = 0;
   return 0;
 }
 
 template class DiffTrace<DiffTestState>;
+
+#ifdef CONFIG_IOTRACE_ZSTD
+void DiffTraceZstd::diff_zstd_next(const char *file_name, bool is_read) {
+  if (io_trace_file.is_open()) {
+    io_trace_file.close();
+  }
+  if (is_read) {
+    io_trace_file.open(file_name, std::ios::binary | std::ios::in);
+    if (io_trace_file.is_open() == false) {
+      printf("Run %s not find,No more trace files.End simulation\n", file_name);
+      exit(0);
+    }
+  } else {
+    io_trace_file.open(file_name, std::ios::binary | std::ios::out);
+  }
+}
+
+void DiffTraceZstd::diff_IOtrace_dump(const char *str, uint64_t len) {
+  static const size_t cLevel = 1; // compression level
+
+  std::vector<char> outputBuffer(max_compress_size);
+  trace_cctx = ZSTD_createCCtx();
+
+  size_t compressedSize = ZSTD_compressCCtx(trace_cctx, outputBuffer.data(), outputBuffer.size(), str, len, cLevel);
+  if (ZSTD_isError(compressedSize)) {
+    std::cerr << "Zstd Compress error: " << ZSTD_getErrorName(compressedSize) << std::endl;
+    ZSTD_freeCCtx(trace_cctx);
+    assert(0);
+    return;
+  }
+
+  io_trace_file.write(outputBuffer.data(), compressedSize);
+  ZSTD_freeCCtx(trace_cctx);
+  trace_cctx = NULL;
+}
+
+bool DiffTraceZstd::diff_IOtrace_load(char *buffer, uint64_t len) {
+  int result = diff_IOtrace_ZstdDcompress();
+  if (result != 0) {
+    need_load_new_file = true;
+    return false;
+  } else {
+    uint64_t have_size = io_trace_buffer.size() / len;
+    uint64_t byte_size = have_size * len;
+    memcpy(buffer, io_trace_buffer.data(), byte_size);
+    trace_load_len = have_size;
+    // clear read data
+    io_trace_buffer.erase(io_trace_buffer.begin(), io_trace_buffer.begin() + byte_size);
+  }
+  return true;
+}
+
+int DiffTraceZstd::diff_IOtrace_ZstdDcompress() {
+  // Set up buffers
+  static const size_t inbufferSize = ZSTD_DStreamInSize(); // Use ZSTD's recommended output buffer size
+  static const size_t outbufferSize = ZSTD_DStreamOutSize();
+  static std::vector<char> inputBuffer(inbufferSize);
+  std::vector<char> outputBuffer(outbufferSize);
+
+  if (trace_dctx == NULL) {
+    trace_dctx = ZSTD_createDCtx();
+  }
+  // Read and decompress data in a loop
+  ZSTD_outBuffer output = {outputBuffer.data(), outbufferSize, 0};
+  static ZSTD_inBuffer input = {inputBuffer.data(), 0, 0};
+
+  if (input.pos == input.size) {
+    inputBuffer.resize(inbufferSize);
+    io_trace_file.read(inputBuffer.data(), inbufferSize);
+    input.size = io_trace_file.gcount();
+    input.pos = 0;
+
+    // Outputs the current file pointer location
+    std::streampos currentPos = io_trace_file.tellg();
+    if (currentPos == -1) {
+      std::cout << "Decompress read zstd file error" << std::endl;
+      return 2;
+    }
+  } else if (input.size == 0) {
+    ZSTD_freeDCtx(trace_dctx);
+    trace_dctx = NULL;
+    return 1;
+  } else {
+    input.size = input.size;
+    input.pos = input.pos;
+  }
+
+  // Decompress the data
+  size_t ret = ZSTD_decompressStream(trace_dctx, &output, &input);
+
+  io_trace_buffer.insert(io_trace_buffer.end(), outputBuffer.begin(), outputBuffer.end());
+
+  return 0;
+}
+#endif // CONFIG_IOTRACE_ZSTD
+
 #ifdef CONFIG_DIFFTEST_IOTRACE
 template class DiffTrace<DiffTestIOTrace>;
 #endif // CONFIG_DIFFTEST_IOTRACE
--- src/test/csrc/difftest/difftrace.h
+++ src/test/csrc/difftest/difftrace.h
@@ -5,11 +5,56 @@
 #ifdef CONFIG_DIFFTEST_IOTRACE
 #include "difftest-iotrace.h"
 #endif // CONFIG_DIFFTEST_IOTRACE
+#ifdef CONFIG_IOTRACE_ZSTD
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <zstd.h>
+#endif // CONFIG_IOTRACE_ZSTD
+
+#ifdef CONFIG_IOTRACE_ZSTD
+class DiffTraceZstd {
+public:
+  int trace_load_len = 0;
+  bool need_load_new_file = true;
+
+  DiffTraceZstd(uint64_t buffer_size) {
+    trace_buffer_size = buffer_size;
+    max_compress_size = 5000 * trace_buffer_size / 10;
+    max_dcompress_size = 5000 * trace_buffer_size;
+    io_trace_buffer.reserve(max_dcompress_size);
+  };
+
+  ~DiffTraceZstd() {
+    ZSTD_freeCCtx(trace_cctx);
+  }
+
+  void diff_zstd_next(const char *file_name, bool is_read);
+
+  void diff_IOtrace_dump(const char *str, uint64_t len);
+
+  bool diff_IOtrace_load(char *buffer, uint64_t len);
+  int diff_IOtrace_ZstdDcompress();
+
+private:
+  uint64_t trace_buffer_size = 0;
+  uint64_t max_compress_size = 0; // The number of bytes in a single compression
+  uint64_t max_dcompress_size = 0;
+  std::vector<char> io_trace_buffer;
+
+  ZSTD_CCtx *trace_cctx = NULL;
+  ZSTD_DCtx *trace_dctx = NULL;
+  std::fstream io_trace_file;
+};
+#endif // CONFIG_IOTRACE_ZSTD
 
 template <typename T> class DiffTrace {
 public:
   char trace_name[32];
   bool is_read;
+#ifdef CONFIG_IOTRACE_ZSTD
+  DiffTraceZstd *trace_zstd = NULL;
+#endif // CONFIG_IOTRACE_ZSTD
 
   DiffTrace(const char *trace_name, bool is_read, uint64_t buffer_size = 1024 * 1024);
   ~DiffTrace() {
@@ -19,9 +64,13 @@ public:
     if (buffer) {
       free(buffer);
     }
+#ifdef CONFIG_IOTRACE_ZSTD
+    delete trace_zstd;
+#endif // CONFIG_IOTRACE_ZSTD
   }
   bool append(const T *trace);
   bool read_next(T *trace);
+  void next_file_name(char *file_name);
 
 private:
   uint64_t buffer_size;
