74cb8f01e843a41906d2dc88aa39e21ed30babdc
klin02 <youkunlin20@mails.ucas.ac.cn>
2025-01-31 13:55:52 +0800

    feat(query): support query for difftest-dpic data

--- libso.mk
+++ libso.mk
@@ -4,7 +4,7 @@ CC_OBJ_DIR	 = $(abspath $(BUILD_DIR)/cc_obj)
 LIB_CSRC_DIR 	 = $(abspath ./src/test/csrc/vcs)
 LIB_CXXFILES 	 = $(SIM_CXXFILES) $(shell find $(LIB_CSRC_DIR) -name "*.cpp")
 LIB_CXXFLAGS 	 = -m64 -c -fPIC -g -std=c++11
-LIB_CXXFLAGS 	+= $(subst \\\",\", $(SIM_CXXFLAGS)) -I$(LIB_CSRC_DIR) -DNUM_CORES=$(NUM_CORES)
+LIB_CXXFLAGS 	+= $(subst \\\",\", $(SIM_CXXFLAGS)) $(SIM_LDFLAGS) -I$(LIB_CSRC_DIR) -DNUM_CORES=$(NUM_CORES)
 
 ifeq ($(WITH_DRAMSIM3),1)
 LD_LIB  	 = -L $(DRAMSIM3_HOME)/ -ldramsim3 -Wl,-rpath-link=$(DRAMSIM3_HOME)/libdramsim3.so
