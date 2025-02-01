b3dabd511cf6c22aae3f0e7d28c8acda696e68d3
Kunlin You <youkunlin20@mails.ucas.ac.cn>
2025-02-01 20:29:54 +0800

    Batch: collect from different groups in one cycle to reduce gates (#558)
    Previous we collect data from different groups(a group means Diffstate
    with same desireCppName) in pipeline (delay different groups to different
    stage by RegNext). And at each stage Batch will concat valid data
    from one group to step_data.
    
    This change use two-stage collector to reduce gates:
    Stage1: Different groups will collect data in parallel to get
    tight-arrangement group_data.
    Stage2: Batch will collect data from different groups in one cycle,
    group_vsize will be used to cut valid data from tight group_data by
    LookupTree, and valid data from different groups will be concated to
    step_data.
    
    In pallaium, this change will reduce Batch gates from 21M to 12M.
    And in FPGA, XS(Basic-Diff) with ESBI config will use 80% LUT (XS needs
    70% LUT itself).

--- src/main/scala/Gateway.scala
+++ src/main/scala/Gateway.scala
@@ -54,6 +54,7 @@ case class GatewayConfig(
   def dutBufLen: Int = if (isBatch) batchSize else 1
   def maxStep: Int = if (isBatch) batchSize else 1
   def stepWidth: Int = log2Ceil(maxStep + 1)
+  def replayWidth: Int = log2Ceil(replaySize + 1)
   def batchArgByteLen: (Int, Int) = if (isNonBlock || isFPGA) (3600, 400) else (7200, 800)
   def hasDeferredResult: Boolean = isNonBlock || hasInternalStep
   def needTraceInfo: Boolean = hasReplay
