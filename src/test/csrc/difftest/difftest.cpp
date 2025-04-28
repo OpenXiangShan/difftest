23bbd428c3adedabfdec1512ca796fb88b6702f7
sfencevma <15155930562@163.com>
2025-04-28 21:56:28 +0800

    fix(Difftest): fix some syntax errors

--- src/test/csrc/difftest/difftest.cpp
+++ src/test/csrc/difftest/difftest.cpp
@@ -1710,9 +1710,11 @@ void DiffState::display(int coreid) {
   Info("\n============== Commit Group Trace (Core %d) ==============\n", coreid);
   int group_index = 0;
   while (!retire_group_queue.empty()) {
-    auto retire_group = retire_group_queue.front()
+    auto retire_group = retire_group_queue.front();
+    auto pc = retire_group.first;
+    auto cnt = retire_group.second;
     retire_group_queue.pop();
-    Info("commit group [%02d]: pc %010lx cmtcnt %d%s\n", group_index, retire_group.pc, retire_group.cnt,
+    Info("commit group [%02d]: pc %010lx cmtcnt %d%s\n", group_index, pc, cnt,
          retire_group_queue.empty() ? " <--" : "");
     group_index++;
   }
