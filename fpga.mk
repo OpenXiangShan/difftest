f832063190390f26ce118b35d6eb01a3629a5d25
Kamimiao <fengkehan@bosc.ac.cn>
2025-07-10 14:28:50 +0800

    fix(fpga.mk): specify c++11 option (#635)

--- fpga.mk
+++ fpga.mk
@@ -6,7 +6,7 @@ DMA_CHANNELS ?= 1
 
 FPGA_CXXFILES  = $(SIM_CXXFILES) $(shell find $(FPGA_CSRC_DIR) -name "*.cpp")
 FPGA_CXXFLAGS  = $(subst \\\",\", $(SIM_CXXFLAGS)) -I$(FPGA_CSRC_DIR) -DCONFIG_DMA_CHANNELS=$(DMA_CHANNELS) -DNUM_CORES=$(NUM_CORES) -DCONFIG_PLATFORM_FPGA
-FPGA_CXXFLAGS += -O3 -flto -march=native -mtune=native
+FPGA_CXXFLAGS += -std=c++11 -O3 -flto -march=native -mtune=native
 FPGA_LDFLAGS   = $(SIM_LDFLAGS) -lpthread -ldl
 
 fpga-build: fpga-clean fpga-host
