45e6966779c0f8ac75a2a339b9742a86825e401f
Jiuyue Ma <majiuyue@bosc.ac.cn>
2025-07-10 17:15:53 +0800

    fix(pdb.mk): Add `PDB_` prefix to variables to avoid conflit with libso.mk
    Signed-off-by: Jiuyue Ma <majiuyue@bosc.ac.cn>

--- pdb.mk
+++ pdb.mk
@@ -1,40 +1,40 @@
 PYTHON_DIR       = $(abspath $(BUILD_DIR)/xspdb/python)
-CC_OBJ_DIR       = $(abspath $(BUILD_DIR)/xspdb/swig_obj)
+PDB_OBJ_DI       = $(abspath $(BUILD_DIR)/xspdb/swig_obj)
 
 picker_include   = $(shell picker --show_xcom_lib_location_cpp|grep include|awk '{print $$2}')
 
-LIB_SWIG_DIR     = $(abspath src/test/csrc/plugin/xspdb)
-LIB_CXXFILES     = $(SIM_CXXFILES) $(shell find $(LIB_SWIG_DIR)/cpp -name "*.cpp") $(shell find $(SIM_CONFIG_DIR) -name "*.cpp")
-LIB_CXXFLAGS 	+= $(subst \\\",\", $(SIM_CXXFLAGS)) -DNUM_CORES=$(NUM_CORES)
+PDB_SWIG_DIR     = $(abspath src/test/csrc/plugin/xspdb)
+PDB_CXXFILES     = $(SIM_CXXFILES) $(shell find $(PDB_SWIG_DIR)/cpp -name "*.cpp") $(shell find $(SIM_CONFIG_DIR) -name "*.cpp")
+PDB_CXXFLAGS    += $(subst \\\",\", $(SIM_CXXFLAGS)) -DNUM_CORES=$(NUM_CORES)
 
 ifeq ($(WITH_DRAMSIM3),1)
-LD_LIB  	 = -L $(DRAMSIM3_HOME)/ -ldramsim3 -Wl,-rpath-link=$(DRAMSIM3_HOME)/libdramsim3.so
+PDB_LD_LIB       = -L $(DRAMSIM3_HOME)/ -ldramsim3 -Wl,-rpath-link=$(DRAMSIM3_HOME)/libdramsim3.so
 endif
 
-LIB_CXXFLAGS    += -I$(VCS_HOME)/include $(shell python3-config --includes) -I$(LIB_SWIG_DIR)/cpp
-LD_LIB          += $(shell python3-config --ldflags)
+PDB_CXXFLAGS    += -I$(VCS_HOME)/include $(shell python3-config --includes) -I$(LIB_SWIG_DIR)/cpp
+PDB_LD_LIB      += $(shell python3-config --ldflags)
 
 
-SWIG_INCLUDE     = $(filter -I%, $(LIB_CXXFLAGS)) -I$(picker_include)
+SWIG_INCLUDE     = $(filter -I%, $(PDB_CXXFLAGS)) -I$(picker_include)
 ifeq ($(WITH_CHISELDB), 1)
   SWIG_D        += -DENABLE_CHISEL_DB
 endif
 
 process_DiffTestState:
-	rm -rf $(CC_OBJ_DIR) $(PYTHON_DIR)
-	mkdir -p $(CC_OBJ_DIR)
+	rm -rf $(PDB_OBJ_DIR) $(PYTHON_DIR)
+	mkdir -p $(PDB_OBJ_DIR)
 	mkdir -p $(PYTHON_DIR)
-	cp $(LIB_SWIG_DIR)/swig.i $(CC_OBJ_DIR)/python.i
-	echo "%extend DiffTestState {" >> $(CC_OBJ_DIR)/python.i
-	cat $(BUILD_DIR)/generated-src/diffstate.h|grep Difftest|grep "\["|sed "s/\[/\ /g"|sed "s/\]/\ /g"|awk '{print $$1 " *get_"$$2"(int index){if(index<"$$3"){return &(self->"$$2"[index]);} return NULL;}"}' >> $(CC_OBJ_DIR)/python.i
-	echo "}" >> $(CC_OBJ_DIR)/python.i
+	cp $(PDB_SWIG_DIR)/swig.i $(PDB_OBJ_DIR)/python.i
+	echo "%extend DiffTestState {" >> $(PDB_OBJ_DIR)/python.i
+	cat $(BUILD_DIR)/generated-src/diffstate.h|grep Difftest|grep "\["|sed "s/\[/\ /g"|sed "s/\]/\ /g"|awk '{print $$1 " *get_"$$2"(int index){if(index<"$$3"){return &(self->"$$2"[index]);} return NULL;}"}' >> $(PDB_OBJ_DIR)/python.i
+	echo "}" >> $(PDB_OBJ_DIR)/python.i
 
 swig_export: process_DiffTestState
-	swig -c++ -outdir $(PYTHON_DIR) -o $(CC_OBJ_DIR)/difftest_wrap.cpp $(SWIG_INCLUDE) -python $(SWIG_D) $(CC_OBJ_DIR)/python.i
-	$(eval LIB_CXXFILES = $(LIB_CXXFILES) $(CC_OBJ_DIR)/difftest_wrap.cpp)
+	swig -c++ -outdir $(PYTHON_DIR) -o $(PDB_OBJ_DIR)/difftest_wrap.cpp $(SWIG_INCLUDE) -python $(SWIG_D) $(PDB_OBJ_DIR)/python.i
+	$(eval PDB_CXXFILES = $(PDB_CXXFILES) $(PDB_OBJ_DIR)/difftest_wrap.cpp)
 
 difftest_python: swig_export
-	cd $(CC_OBJ_DIR) 					&& \
-	$(CC) $(LIB_CXXFLAGS) $(LIB_CXXFILES)			&& \
-	$(CC) -o $(PYTHON_DIR)/_difftest.so -m64 -shared *.o $(LD_LIB) $(SIM_LDFLAGS)
+	cd $(PDB_OBJ_DIR) 					&& \
+	$(CC) $(PDB_CXXFLAGS) $(PDB_CXXFILES)			&& \
+	$(CC) -o $(PYTHON_DIR)/_difftest.so -m64 -shared *.o $(PDB_LD_LIB) $(SIM_LDFLAGS)
 	echo $(SIM_LDFLAGS) > $(PYTHON_DIR)/sim_ld_flags.txt
