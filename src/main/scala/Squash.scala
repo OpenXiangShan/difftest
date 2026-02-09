e66e60c483cdbfbbdbfbb09834b7b4cf8e0bcfc5
klin02 <youkunlin20@mails.ucas.ac.cn>
2026-02-09 21:18:25 +0800

    fix: tick squash only when ready

--- src/main/scala/Squash.scala
+++ src/main/scala/Squash.scala
@@ -162,7 +162,8 @@ class SquashEndpoint(bundles: Seq[Valid[DifftestBundle]], config: GatewayConfig)
         .zip(group_tick_vec)
         .collect { case (n, gt) if u.squashGroup.contains(n) => gt }
         .foldLeft(false.B)(_ || _)
-    squasher.should_tick := wt || group_tick || global_tick
+    // Only tick when out.ready to avoid output missing
+    squasher.should_tick := (wt || group_tick || global_tick) && pipelined.ready
     squasher.out
   }
   // Flatten Seq[MixedVec[DifftestBundle]] to MixedVec[DifftestBundle]
