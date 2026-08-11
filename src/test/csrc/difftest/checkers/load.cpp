d27746b515b849ad78b36ddeaaffc73316f33359
Wang Huizhe <huizhewang@outlook.com>
2026-08-11 16:43:05 +0800

    Revert "feat(difftest): support RISC-V Zabha (#918)"
    This reverts commit bd5a0273c375cf1c38b3f5ca10e347e095a7aef1.

--- src/test/csrc/difftest/checkers/load.cpp
+++ src/test/csrc/difftest/checkers/load.cpp
@@ -204,13 +204,10 @@ int LOADCHECKCLASS::do_load_check(const DifftestLoadEvent &probe, bool regWen, u
           default: Info("Unknown fuOpType: 0x%x\n", probe.opType); return STATE_ERROR;
         }
       } else if (probe.isAtomic) {
-        switch (probe.opType & 0x7) {
-          case 0: len = 1; break; // B
-          case 1: len = 2; break; // H
-          case 2: len = 4; break; // W
-          case 3: len = 8; break; // D
-          case 4: len = 8; break; // Q is committed one XLEN chunk at a time
-          default: Info("Unknown atomic size: 0x%x\n", probe.opType); return STATE_ERROR;
+        if (probe.opType % 2 == 0) {
+          len = 4;
+        } else { // probe.opType % 2 == 1
+          len = 8;
         }
       }
       read_goldenmem(probe.paddr, &golden, len, &golden_flag);
